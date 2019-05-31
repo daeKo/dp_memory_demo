fdac0787741e0814a5b548f804b8fa9130616580
Daniel Kowatsch <kowatscd@in.tum.de>
2019-05-31 02:40:41 +0200

    Added requirements and setup file

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Creating virtual environment.."
+python -m venv --p Python3 .venv
+
+echo "Installing requirements.."
+source .venv/bin/activate
+pip install -r requirements.txt
+
+echo "Installing virtual environment as ipykernel..."
+python -m ipykernel install --user --name .venv --display-name "Python (DP Memory Example)"
+
+echo "Installation complete, kernel should be installed with name 'Python (DP Memory Example)'"
